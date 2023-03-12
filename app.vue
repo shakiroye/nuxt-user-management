97ebff66d0f8b1c69db2bd4900c5a922229e62a5
shakiroye <oyeossiadewoye@gmail.com>
2023-03-12 22:06:50 +0000

    add components

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
+<script setup>
+import Account from "./components/Account.vue";
+import Auth from "./components/Auth.vue";
+
+const user = useSupabaseUser();
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="user" />
+    <Auth v-else />
   </div>
 </template>
